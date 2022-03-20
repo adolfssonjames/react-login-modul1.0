8fdfd697b2066a90a93e1d1262e4ebb4934b680a
James Adolfsson <adolfssonjames@gmail.com>
2022-03-21 00:33:49 +0100

    lite comments

--- src/Components/FormOk.jsx
+++ src/Components/FormOk.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './Style.css';
-
-
-
-const FormOk = () => {
-   
-  return (
- 
-    <div className="content-form-ok">
-        <h1 className='acc'>Account Created, Welcome!</h1>
-        <p> You are now logged in! Enjoy your new account :)</p>
-        <img className='welcome-gif' src="tomjerry.webp" alt="" /> 
-    </div>
-  );
-};
-  
-  export default FormOk;
+//import React from 'react';
+//import './Style.css';
+//
+//
+//
+//const FormOk = () => {
+//   
+//  return (
+// 
+//    <div className="content-form-ok">
+//        <h1 className='acc'>Account Created, Welcome!</h1>
+//        <p> You are now logged in! Enjoy your new account :)</p>
+//        <img className='welcome-gif' src="tomjerry.webp" alt="" /> 
+//    </div>
+//  );
+//};
+//  
+//  export default FormOk;
+//
